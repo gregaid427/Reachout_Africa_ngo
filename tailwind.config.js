a6b4caf7e016a197a1de8fba0a635de229e8335b
Gregory Aidoo <gregaid427@gmail.com>
2022-09-11 17:18:50 +0000

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
-  theme: {
-  //  screens: {"sm":"400px"},
-  },
-  plugins: [],
-}
+
+    theme: {
+      screens: {
+        'sm': { 'max': '480px'},
+        // => @media (min-width: 640px and max-width: 767px) { ... }
+  
+        'md': {'min': '481px', 'max': '768px'},
+        // => @media (min-width: 768px and max-width: 1023px) { ... }
+  
+        'lg': {'min': '789px', 'max': '1024px'},
+        // => @media (min-width: 1024px and max-width: 1279px) { ... }
+  
+        'xl': {'min': '1029px', 'max': '1279px'},
+        // // => @media (min-width: 1280px and max-width: 1535px) { ... }
+  
+        // '2xl': {'min': '1536px'},
+        // // => @media (min-width: 1536px) { ... }
+      },
+    }, plugins: []
+  }
+ 
+
+// 'min': '320px',
